d086716fe086dafd31d8c99e717d5813fc7236f9
José R. <124866471+silvaleal@users.noreply.github.com>
2025-06-08 08:17:50 -0300

    Update auth.php

--- app/Helpers/auth.php
+++ app/Helpers/auth.php
@@ -6,6 +6,9 @@ class Auth {
     public static function check() {
         if (isset($_SESSION['auth'])) return true;
     }
+    public static function get() {
+        if (isset($_SESSION['auth'])) return $_SESSION['auth'][0];
+    }
 
     public static function set(array $user) {
         $_SESSION['auth'] = $user;
